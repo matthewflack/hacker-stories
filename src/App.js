07fc676b951e88a1944132c211f78e5c30f62618
Matthew Flack <maq.flack@gmail.com>
2021-04-08 15:28:40 -0700

    Add input onChange function

--- src/App.js
+++ src/App.js
@@ -20,17 +20,21 @@ const list = [
 ]
 
 
-const App= () => (
+const App= () => {
+
+const handleChange = event=>{
+  console.log(event.target.value);
+}
+
+return(
     <div>
       <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search</label>
-      <input id='search' type='text'/>
+      <input onChange ={handleChange} id='search' type='text'/>
       <hr/>
-      
         <List/>
-      
     </div>
-  );
+  )};
 
 const List= () => (
   <ul>
