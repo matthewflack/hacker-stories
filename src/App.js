205e444fa68e0431a158026dcba024dcec8dff0f
Matthew Flack <maq.flack@gmail.com>
2021-04-13 16:20:38 -0700

    Refactor <Search/> to be more reusable

--- src/App.js
+++ src/App.js
@@ -53,8 +53,13 @@ return(
     <div>
       <h1>My Hacker Stories</h1>
         
-        <Search search={searchTerm} onSearch={handleSearch} />
-     
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
+
       <hr/>
       
         <List list = {searchedStories}/>
@@ -63,20 +68,21 @@ return(
 
 {/*------------------------------------------------------ */}
 
-const Search = ({search, onSearch}) => 
-(
+const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => (
   <>
-    <label htmlFor="search">Search</label>
-    <input 
-      id='search' 
-      type='text' 
-      value ={search}
-      onChange ={onSearch} 
+    <label htmlFor={id}>{label}</label>
+    &nbsp;
+    <input
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
     />
   </>
- );
+  );
 
 {/*------------------------------------------------------ */}
+
 
 const List= ({list}) => (
   list.map(item => <Item key={item.objectID} item ={item} />)
