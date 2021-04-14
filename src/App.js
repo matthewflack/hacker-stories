18fed2d31a0c91db994d4fe3785d1db922b77ea4
Matthew Flack <maq.flack@gmail.com>
2021-04-14 14:13:13 -0700

    Make list of stories stateful.  Pass down as props to <List/> and add callback handler (handleRemoveStory) / (handleRemoveItem) to be used in components

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const useSemiPersistentState = (key, initialState) => {
 
 const App= () => {
 
-  const stories = [
+  const initialStories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -38,7 +38,16 @@ const App= () => {
       }
   ];
  
+
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search','React');
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = item => {
+      const newStories = stories.filter(
+        story => item.objectID !== story.objectID
+      );
+      setStories(newStories);
+    };
 
   const handleSearch = event =>{
     setSearchTerm(event.target.value);
@@ -63,7 +72,7 @@ return(
       </InputWithLabel>
       <hr/>
       
-        <List list = {searchedStories}/>
+        <List list = {searchedStories} onRemoveItem={handleRemoveStory}/>
     </div>
   )};
 
@@ -105,13 +114,21 @@ const InputWithLabel = ({ id, value, type = 'text',isFocused, onInputChange, chi
 {/*------------------------------------------------------ */}
 
 
-const List= ({list}) => (
-  list.map(item => <Item key={item.objectID} item ={item} />)
+const List= ({list, onRemoveItem}) => (
+  list.map(item => <Item 
+    key={item.objectID} 
+    item ={item} 
+    onRemoveItem={onRemoveItem}
+    />)
   );
 
 {/*------------------------------------------------------ */}
 
-const Item =({item})=> (
+const Item =({item,onRemoveItem})=>{ 
+const handleRemoveItem = ()=> {
+  onRemoveItem(item);
+}
+return(
   <div>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -119,8 +136,13 @@ const Item =({item})=> (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+    <button type="button" onClick={handleRemoveItem}>
+      Dismiss
+    </button>
+    </span>
   </div>
-);
+)};
 
 {/*------------------------------------------------------ */}
 
