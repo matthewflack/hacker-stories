fcf35da922e1e471864a9b523e81d59730eee8c7
Matthew Flack <maq.flack@gmail.com>
2021-04-16 20:37:01 -0700

    Add conditional rendering for states of isLoading / isError in <App/>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-{/*Completed up to pg 58.... need to commit / add that work.*/} 
+{/*Completed up to pg 88.... need to commit / add that work.*/} 
 
 {/*------------------------------------------------------ */}
 
@@ -48,11 +48,18 @@ const getAsyncStories = () =>
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search','React');
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
+
+    setIsLoading(true);
     getAsyncStories().then(result => {
     setStories(result.data.stories);
-    });
+    setIsLoading(false);
+    })
+    .catch(()=>setIsError(true));
+    ;
     }, []);
 
   const handleRemoveStory = item => {
@@ -84,8 +91,12 @@ return(
         <strong>Search</strong>
       </InputWithLabel>
       <hr/>
-      
+      {isError && <p>Something went wrong ...</p>}
+      {isLoading ? (<p>Loading...</p>) : (
+
         <List list = {searchedStories} onRemoveItem={handleRemoveStory}/>
+
+      )}
     </div>
   )};
 
