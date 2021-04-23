4ef61d960177d57c801ec8df70aca4a9fe3aab22
Matthew Flack <maq.flack@gmail.com>
2021-04-22 21:46:57 -0700

    Remove initialStores array & getAsyncStores() and use fetch to get data from Hacker News API

--- src/App.js
+++ src/App.js
@@ -3,32 +3,7 @@ import React from 'react';
 {/*Completed up to pg 92.... need to commit / add that work.*/} 
 
 {/*------------------------------------------------------ */}
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-    },
-    {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-    }
-];
 
-const getAsyncStories = () =>
-  new Promise(resolve =>
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  );
 
 
 const useSemiPersistentState = (key, initialState) => {
@@ -75,6 +50,8 @@ const storiesReducer = (state, action) => {
     }
   };
 
+  const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
 {/*------------------------------------------------------ */}
 
 const App= () => {
@@ -85,18 +62,20 @@ const App= () => {
   
  
   React.useEffect(() => {
-    dispatchStories({type: 'STORIES_FETCH_INIT'});
-    
-    getAsyncStories()
-    .then(result => {
-      dispatchStories({
-        type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories,
-      });
-    })
-    .catch(()=>
-    dispatchStories({type: 'STORIES_FETCH_FAILURE'}));
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+    fetch(`${API_ENDPOINT}react`) // B
+      .then(response => response.json()) // C
+      .then(result => {
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.hits, // D
+        });
+       })
+    .catch(() =>
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+    );
     }, []);
+
 
   const handleRemoveStory = item => {
     dispatchStories({
