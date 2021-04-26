8233ca73d03099f5ea638d4dcb8e2bbb44c9d755
Matthew Flack <maq.flack@gmail.com>
2021-04-26 14:14:20 -0700

    Import styled-components and convert to use them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import styled from 'styled-components';
 
 {/*Completed up to pg 92.... need to commit / add that work.*/} 
 
+{/*------------------ Styled Components--------------------------- */}
+
+const StyledContainer = styled.div`
+height: 100vw;
+padding: 20px;
+background: #83a4d4;
+background: linear-gradient(to left, #b6fbff, #83a4d4);
+color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+font-size: 48px;
+font-weight: 300;
+letter-spacing: 2px;
+`;
+
+const StyledItem = styled.div`
+display: flex;
+align-items: center;
+padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+padding: 0 5px;
+white-space: nowrap;
+overflow: hidden;
+white-space: nowrap;
+text-overflow: ellipsis;
+a {
+color: inherit;
+}
+width: ${props => props.width};
+`;
+
+const StyledButton = styled.button`
+background: transparent;
+border: 1px solid #171212;
+padding: 5px;
+cursor: pointer;
+transition: all 0.1s ease-in;
+&:hover {
+background: #171212;
+color: #ffffff;
+}`;
+
+const StyledButtonSmall = styled(StyledButton)`
+padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+padding: 10px 0 20px 0;
+display: flex;
+align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+border-top: 1px solid #171212;
+border-left: 1px solid #171212;
+padding-left: 5px;
+font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+border: none;
+border-bottom: 1px solid #171212;
+background-color: transparent;
+font-size: 24px;
+`;
+
 {/*------------------------------------------------------ */}
-
-
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -102,8 +174,8 @@ const App= () => {
 
 return(
     
-  <div className="container">
-  <h1 className="headline-primary">My Hacker Stories</h1>
+  <StyledContainer>
+    <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
 
       <SearchForm 
         searchTerm={searchTerm}
@@ -118,7 +190,7 @@ return(
         <List list = {stories.data} onRemoveItem={handleRemoveStory}/>
 
       )}
-    </div>
+    </StyledContainer>
   )};
 
 {/*------------------------------------------------------ */}
@@ -127,7 +199,7 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit,
   }) => (
-  <form onSubmit={onSearchSubmit} className="search-form">
+  <StyledSearchForm onSubmit={onSearchSubmit}>
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -138,10 +210,10 @@ const SearchForm = ({
         Search:
       </strong>
     </InputWithLabel>
-    <button type="submit" disabled={!searchTerm} className="button button_large">
+    <StyledButtonLarge type="submit" disabled={!searchTerm}>
       Submit
-    </button>
-  </form>
+    </StyledButtonLarge>
+</StyledSearchForm>
   )
 {/*-------------------------------------------------------*/}
 
@@ -161,22 +233,20 @@ const InputWithLabel = ({ id, value, type = 'text',isFocused, onInputChange, chi
     }, [isFocused]
   );
     
-    return(
+  return (
     <>
-      <label htmlFor={id} className="label">{children}</label>
+      <StyledLabel htmlFor={id}>{children}</StyledLabel>
       &nbsp;
-      {/* B */}
-      <input
+      <StyledInput
         ref={inputRef}
         id={id}
         type={type}
         value={value}
-        autoFocus={isFocused}
         onChange={onInputChange}
-        className="input"
       />
     </>
-  )};
+    );
+  };
 
 {/*------------------------------------------------------ */}
 
@@ -191,29 +261,24 @@ const List= ({list, onRemoveItem}) => (
 
 {/*------------------------------------------------------ */}
 
-const Item =({item,onRemoveItem})=>{ 
-const handleRemoveItem = ()=> {
-  onRemoveItem(item);
-}
-return(
-  <div className="item">
-    <span style = {{width: '40%'}}>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span style = {{width: '30%'}}>{item.author}</span>
-    <span style = {{width: '10%'}}>{item.num_comments}</span>
-    <span style = {{width: '10%'}}>{item.points}</span>
-    <span style = {{width: '10%'}}>
-    <button type="button" 
-    onClick={handleRemoveItem}
-    className="button button_small"
+const Item = ({ item, onRemoveItem }) => (
+<StyledItem>
+  <StyledColumn width="40%">
+    <a href={item.url}>{item.title}</a>
+  </StyledColumn>
+  <StyledColumn width="30%">{item.author}</StyledColumn>
+  <StyledColumn width="10%">{item.num_comments}</StyledColumn>
+  <StyledColumn width="10%">{item.points}</StyledColumn>
+  <StyledColumn width="10%">
+    <StyledButtonSmall
+      type="button"
+      onClick={() => onRemoveItem(item)}
     >
-    
       Dismiss
-    </button>
-    </span>
-  </div>
-)};
+    </StyledButtonSmall>
+  </StyledColumn>
+</StyledItem>
+);
 
 {/*------------------------------------------------------ */}
 
